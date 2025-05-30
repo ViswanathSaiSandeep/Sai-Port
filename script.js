4660fbd613228898a2492fedfffd6ff76a3a37b0
ViswanathSaiSandeep <viswanathsaisandeep4029.sse@saveetha.com>
2025-05-30 21:09:28 +0530

    animation

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleMenu(){
     document.getElementById("nav-item").classList.toggle("show")
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed('#typed-text', {
+    strings: ["Hey I'm", "Welcome!", "Glad you're here!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    loop: true
+  });
+});
